711b0211531633751c4485f2cf5b2bacc1f1d085
Hadeer Ramadan <hadderramadan7@gmail.com>
2024-12-25 21:59:36 +0200

    final virsion

--- preprocessing_models.R
+++ preprocessing_models.R
@@ -1,75 +1,66 @@
 # installing the needed libraries 
 
 
+# Function to remove outliers
+remove_outliers <- function(x) {
+  Q1 <- quantile(x, 0.25, na.rm = TRUE)
+  Q3 <- quantile(x, 0.75, na.rm = TRUE)
+  IQR_value <- IQR(x, na.rm = TRUE)
+  x[x >= (Q1 - 1.5 * IQR_value) & x <= (Q3 + 1.5 * IQR_value)]
+}
 
+
+
+#_________Instailling needed packages_____________
 install.packages(c("dplyr", "lubridate", "tidyr"))
 install.packages(c("rpart", "rpart.plot"))
 install.packages("e1071")
+install.packages('caret', dependencies = TRUE)
+install.packages("xgboost")
+install.packages("randomForest")
 
 library(rpart)
 library(rpart.plot)
 library(dplyr)
 library(lubridate)
 library(tidyr)
-install.packages('caret', dependencies = TRUE)
-install.packages("xgboost")
-install.packages("caret")
 library(caret)
-install.packages("randomForest")
 library(randomForest)
 library(e1071)
 
+#---------------------------------------------------------------------------------------------------------------------------------------------
 
+#______________Load Data__________________
+used_car_data <- read.csv("used_car_dataset.csv", na.strings="")
 
-# then read the data set 
-used_car_data <- read.csv('used_car_dataset.csv')
-DF<-data.frame()
 View(used_car_data)
-head(used_car_data)
 dim(used_car_data)
 names(used_car_data)
 
-#----------------------------------------------------------------------------------- starting preprocessing --------------------------------------------------
+# Remove Nulls
+colSums(is.na(used_car_data))
 
-# Drop multiple columns together by specifying their indices
-#-----------------------------------------------------------
+used_car_data <- na.omit(used_car_data)
+
+colSums(is.na(used_car_data))
+
+# Drop unuseful columns 
+
 used_car_data <- used_car_data[, -c(3, 7, 9,10)]
 
 
+# repair negatives values in age column
 
-# check if there is any missing values we delete it and it will not affect in the dataset size 
-#---------------------------------------------------------------------------------------------
-#check the number of missing values in each column before deleting 
-missing_values_before <- colSums(is.na(used_car_data))
-print(missing_values_before)
-used_car_data_cleaned <- na.omit(used_car_data)
-# Check the number of missing values in each column after deletion
-missing_values_after <- colSums(is.na(used_car_data_cleaned))
-print(missing_values_after)
+used_car_data$Age[used_car_data$Age<0]<-used_car_data$Age[used_car_data$Age<0]*(-1)
 
 
-#check if there is any duplicated data and handle it
-#---------------------------------------------------
+# remove sign character from kmDriven and AskPrice
 
-
-duplicated_rows <- used_car_data[duplicated(used_car_data), ]
-nrow(duplicated_rows)
-
-# there is 748 dupliactes rows so we will delete them by this 
-used_car_data <- unique(used_car_data)
-dim(used_car_data)
-
-#check again if there is any duplicated data after deleting them all 
-duplicated_rows <- used_car_data[duplicated(used_car_data), ]
-nrow(duplicated_rows)
-
-
-#convert characters
-#--------------------
 used_car_data$kmDriven <- as.numeric(gsub("[ km,]", "", used_car_data$kmDriven))
-
-
 used_car_data$AskPrice <- as.numeric(gsub('[₹,]', '', used_car_data$AskPrice))
+
+
+# convert text to numeric values by label encoding
 
 used_car_data$Brand <- as.factor(used_car_data$Brand)
 used_car_data$Brand <- as.numeric(used_car_data$Brand)
@@ -89,61 +80,61 @@ used_car_data$FuelType <- as.numeric(used_car_data$FuelType)
 #used_car_data$AskPrice <- as.numeric(used_car_data$AskPrice)
 
 
-a<-100
-b<-1000
-
-#used_car_data$Brand <- a+(used_car_data$Brand - min(used_car_data$Brand, na.rm = TRUE))*(b-a) / (max(used_car_data$Brand, na.rm = TRUE) - min(used_car_data$Brand, na.rm = TRUE))
-#used_car_data$model <- a+(used_car_data$model - min(used_car_data$model, na.rm = TRUE))*(b-a) / (max(used_car_data$model, na.rm = TRUE) - min(used_car_data$model, na.rm = TRUE))
-
-#km
-#used_car_data$kmDriven <- round(a+(used_car_data$kmDriven - min(used_car_data$kmDriven, na.rm = TRUE))*(b-a) / (max(used_car_data$kmDriven, na.rm = TRUE) - min(used_car_data$kmDriven, na.rm = TRUE)),0)
-#used_car_data$AskPrice <- round(a+(used_car_data$AskPrice - min(used_car_data$AskPrice, na.rm = TRUE))*(b-a) / (max(used_car_data$AskPrice, na.rm = TRUE) - min(used_car_data$AskPrice, na.rm = TRUE)),0)
-
-#check if any column has any negative value 
-#-------------------------------------------
+#check if there is any duplicated data and handle it
+#---------------------------------------------------
 
 
-# Check for negative values in specific columns
-negative_values <- sapply(used_car_data[, c("Age", "kmDriven", "AskPrice")], function(x) sum(x < 0, na.rm = TRUE))
+duplicated_rows <- used_car_data[duplicated(used_car_data), ]
+nrow(duplicated_rows)
 
-# Print the number of negative values in each of the specified columns
-print(negative_values)
-# number of negative values in each column is 0 so there is no any negative values in the dataset 
+# there is 748 dupliactes rows so we will delete them by this 
+used_car_data <- unique(used_car_data)
+dim(used_car_data)
 
-
-
-
-#KmDriven_counts <- table(used_car_data$kmDriven)
-
-#sorted_kmDriven_counts <- sort(KmDriven_counts, decreasing = TRUE)
-#most_frequent_kmDriven <- names(sorted_kmDriven_counts)[1]
-
-#print(most_frequent_kmDriven)
+#check again if there is any duplicated data after deleting them all 
+duplicated_rows <- used_car_data[duplicated(used_car_data), ]
+nrow(duplicated_rows)
 
 
-#used_car_data$PostedDate <- dmy(used_car_data$PostedDate)
+#___________ Remove oultiers from Age AskPrice KmDriven
 
 
+#kmDriven
+dev.new()
+boxplot(used_car_data$kmDriven, col = rainbow(6), ylab = "kmDriven Boxplot")
+rug(used_car_data$kmDriven,side=2)
 
-#used_car_data$Age[used_car_data$Age<0]<-used_car_data$Age[used_car_data$Age<0]*(-1)   there is no any negative values in this column 
+used_car_data <- used_car_data[!is.na(used_car_data$kmDriven) & used_car_data$kmDriven %in% remove_outliers(used_car_data$kmDriven), ]
 
-
-
-
-#used_car_data$KmDriven <- as.character(used_car_data$KmDriven)
-
-#.str.replace(' km', '').str.replace(',', '').astype(float)
-
-
-
+dev.new()
+boxplot(used_car_data$kmDriven, col = rainbow(6), ylab = "kmDriven Boxplot")
+rug(used_car_data$kmDriven,side=2)
 
 
+#Age
+dev.new()
+boxplot(used_car_data$Age, col = rainbow(6), ylab = "Age Boxplot")
+rug(used_car_data$Age,side=2)
 
-str(used_car_data)
-par(mfrow=c(2,3))
+used_car_data <- used_car_data[!is.na(used_car_data$Age) & used_car_data$Age %in% remove_outliers(used_car_data$used_car_data), ]
+
+dev.new()
+boxplot(used_car_data[-out_in,]$Age, col = rainbow(6), ylab = "Age Boxplot")
+rug(used_car_data$Age,side=2)
+
+#AskPrice
+dev.new()
+boxplot(used_car_data$AskPrice, col = rainbow(6), ylab = "AskPrice Boxplot")
+rug(used_car_data$AskPrice,side=2)
+
+used_car_data <- used_car_data[!is.na(used_car_data$AskPrice) & used_car_data$AskPrice %in% remove_outliers(used_car_data$AskPrice), ]
+
+dev.new()
+boxplot(used_car_data$AskPrice, col = rainbow(6), ylab = "AskPrice Boxplot")
+rug(used_car_data$AskPrice,side=2)
 
 
-#__founding outliers in Age
+# Removing outliers in Age
 dev.new()
 boxplot(used_car_data$Age, col = rainbow(6), ylab = "Age Boxplot")
 rug(used_car_data$Age,side=2)
@@ -157,6 +148,67 @@ dim(used_car_data)
 dev.new()
 boxplot(used_car_data[-out_in,]$Age, col = rainbow(6), ylab = "Age Boxplot")
 rug(used_car_data$Age,side=2)
+#__________________________________________
+
+# Removing outliers from kmDriven
+
+dev.new()
+boxplot(used_car_data$kmDriven, col = rainbow(6), ylab = "kmDriven Boxplot")
+rug(used_car_data$kmDriven,side=2)
+
+kmDriven_out_rm <- boxplot.stats(used_car_data$kmDriven)$out
+kmDriven_out_in <- which(used_car_data$kmDriven %in% c(kmDriven_out_rm))
+
+used_car_data<-used_car_data[-kmDriven_out_in,] # remove outliers
+dim(used_car_data)
+
+dev.new()
+boxplot(used_car_data$kmDriven, col = rainbow(6), ylab = "kmDriven Boxplot")
+rug(used_car_data$kmDriven,side=2)
+
+#__________________________________________
+
+# Removing outliers from AskPrice
+
+dev.new()
+boxplot(used_car_data$AskPrice, col = rainbow(6), ylab = "AskPrice Boxplot")
+rug(used_car_data$AskPrice,side=2)
+
+AskPrice_out_rm <- boxplot.stats(used_car_data$AskPrice)$out
+AskPrice_out_in <- which(used_car_data$AskPrice %in% c(AskPrice_out_rm))
+
+used_car_data<-used_car_data[-AskPrice_out_in,] # remove outliers
+dim(used_car_data)
+
+dev.new()
+boxplot(used_car_data$AskPrice, col = rainbow(6), ylab = "AskPrice Boxplot")
+rug(used_car_data$AskPrice,side=2)
+
+#__________________________________________
+# scalling by min/max method from 0 to 1
+
+#used_car_data$Brand <- (used_car_data$Brand - min(used_car_data$Brand, na.rm = TRUE)) / (max(used_car_data$Brand, na.rm = TRUE) - min(used_car_data$Brand, na.rm = TRUE))
+#used_car_data$model <- (used_car_data$model - min(used_car_data$model, na.rm = TRUE)) / (max(used_car_data$model, na.rm = TRUE) - min(used_car_data$model, na.rm = TRUE))
+#used_car_data$Age <- (used_car_data$Age - min(used_car_data$Age, na.rm = TRUE)) / (max(used_car_data$Age, na.rm = TRUE) - min(used_car_data$Age, na.rm = TRUE))
+
+
+#used_car_data$Transmission <- (used_car_data$Transmission - min(used_car_data$Transmission, na.rm = TRUE)) / (max(used_car_data$Transmission, na.rm = TRUE) - min(used_car_data$Transmission, na.rm = TRUE))
+#used_car_data$FuelType <- (used_car_data$FuelType - min(used_car_data$FuelType, na.rm = TRUE)) / (max(used_car_data$FuelType, na.rm = TRUE) - min(used_car_data$FuelType, na.rm = TRUE))
+
+
+#used_car_data$kmDriven <- (used_car_data$kmDriven - min(used_car_data$kmDriven, na.rm = TRUE))/ (max(used_car_data$kmDriven, na.rm = TRUE) - min(used_car_data$kmDriven, na.rm = TRUE))
+#used_car_data$AskPrice <- (used_car_data$AskPrice - min(used_car_data$AskPrice, na.rm = TRUE))/ (max(used_car_data$AskPrice, na.rm = TRUE) - min(used_car_data$AskPrice, na.rm = TRUE))
+
+
+#a<-1
+#b<-1000
+#used_car_data$kmDriven <- round(a+(used_car_data$kmDriven - min(used_car_data$kmDriven, na.rm = TRUE))*(b-a) / (max(used_car_data$kmDriven, na.rm = TRUE) - min(used_car_data$kmDriven, na.rm = TRUE)),0)
+#used_car_data$AskPrice <- round(a+(used_car_data$AskPrice - min(used_car_data$AskPrice, na.rm = TRUE))*(b-a) / (max(used_car_data$AskPrice, na.rm = TRUE) - min(used_car_data$AskPrice, na.rm = TRUE)),0)
+
+
+# checking structure
+str(used_car_data)
+par(mfrow=c(2,3))
 
 
 
@@ -184,7 +236,6 @@ plot(used_car_data$Transmission, ylab = "Transmission", xlab = "Rank", main = "D
 
 plot(sort(used_car_data$FuelType))
 plot(used_car_data$FuelType, ylab = "FuelType", xlab = "Rank", main = "Distribution of FuelType")
-
 
 plot(sort(used_car_data$AskPrice))
 plot(used_car_data$AskPrice, ylab = "AskPrice", xlab = "Rank", main = "Distribution of AskPrice")
@@ -252,10 +303,9 @@ rug(used_car_data$model,side=2)
 
 
 #____________________________
-
+dev.new()
 boxplot(used_car_data$kmDriven, col = rainbow(6), ylab = "kmDriven Boxplot")
 rug(used_car_data$kmDriven,side=2)
-
 
 boxplot(used_car_data$Transmission, col = rainbow(6), ylab = "Transmission Boxplot")
 rug(used_car_data$Transmission,side=2)
@@ -263,20 +313,22 @@ rug(used_car_data$Transmission,side=2)
 boxplot(used_car_data$FuelType, col = rainbow(6), ylab = "FuelType Boxplot")
 rug(used_car_data$FuelType,side=2)
 
+
 boxplot(used_car_data$AskPrice, col = rainbow(6), ylab = "AskPrice Boxplot")
 rug(used_car_data$AskPrice,side=2)
 
 boxplot(used_car_data$Age, col = rainbow(6), ylab = "Age Boxplot")
 rug(used_car_data$AskPrice,side=2)
-
+dev.new()
 boxplot(used_car_data, col = rainbow(6), ylab = "used_car_data Boxplot")
 
 
 
 
 
-#spliting the dataset into 2 sets 80% into training dataset and the 20% will be the testing dataset
+#______________________________Spliting Data to Train________________________________
 set.seed(123) #34an admn en lt2sem ykon sabt kol mra nsh8l feha el code 34an nfs el result tzhr kol mra
+
 trainIndex <- createDataPartition(used_car_data$AskPrice, p = 0.8, list = FALSE)
 
 train_data <- used_car_data[trainIndex, ]
@@ -290,49 +342,41 @@ test_data <- used_car_data[-trainIndex, ]
 # 1. Linear Regression
 
 
-dim(trainIndex)
-dim(train_data)
-dim(test_data)
-
 # training model 
-regression_model <- lm(train_data$AskPrice ~. , data = train_data)
+linear_model_cars <- lm(AskPrice ~. , data = train_data)
 
-predict_on_train <- predict(regression_model,train_data)
+str(linear_model_cars)
 
+# calculate rmse for trained data
+used_car_reg_rmse_train <- train_data %>%
+  mutate(pred.reg.train = predict(linear_model_cars))
 
-# Add regression line
+View(used_car_reg_rmse_train)
 
 dev.new()
-plot( predict_on_train, train_data$AskPrice, main = "Scatterplot with Regression Line",
-     xlab = predict_on_train, ylab = train_data$AskPrice, pch = 16, col = "blue")
-# Add regression line
-abline(0,1)
+plot(used_car_reg_rmse_train$AskPrice, used_car_reg_rmse_train$pred.reg.train)
+abline(0, 1, col = "red")
 
-#Install Package
-install.packages("hydroGOF")
+mse <- used_car_reg_rmse_train %>%
+  mutate(error = pred.reg.train - AskPrice,
+         sq.error = error^2) %>%
+  summarise(mse = mean(sq.error))
 
-#Load Library
-library(hydroGOF)
+View(mse)
 
-#Calculate RMSE 
-RMSE=rmse(predict_on_train,train_data$AskPrice)
-
-print(RMSE)
-
-str(regression_model)
-dim(train_data)
-
+rmse_train<-sqrt(mse)
+print(rmse_train)
 
 #calculate rmse for test data
 
-pred_reg_test <- predict(regression_model, test_data)
+pred_reg_test <- predict(linear_model_cars, newdata = test_data)
+reg_rmse_test <- sqrt(mean((pred_reg_test - test_data$AskPrice)^2))
 
-rmse_train=rmse(pred_reg_test,test_data$AskPrice)
+r_squared <- cor(test_data$AskPrice, pred_reg_test)^2
+print(r_squared)
+print(reg_rmse_test)
 
 
-r2_lm <- cor(pred_reg_test,test_data$AskPrice)^2
-
-print(r2_lm)
 
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
   #  Trying SVm
@@ -375,7 +419,6 @@ rf_rmse_improved <- sqrt(mean((rf_pred_improved - test_data$AskPrice)^2))
 rf_r2_improved <- cor(rf_pred_improved, test_data$AskPrice)^2
 print(paste("Improved Random Forest R-squared:", round(rf_r2_improved, 3)))
 print(paste("Improved Random Forest RMSE:", format(rf_rmse_improved, scientific = FALSE)))
-
 
 
 ---------------------------------------------------------------------------------------------------------------------------------------------
